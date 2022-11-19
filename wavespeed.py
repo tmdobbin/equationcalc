8a475be2d4fdfb48cc60ac6a2482d8c0c9a77e99
TmDobbin <ryandobson@duck.com>
2022-11-19 21:44:25 +0000

    Add Josh's wavespeed.py

---
+++ wavespeed.py
@@ -0,0 +1,13 @@
+c = 3*10**8 # a constant to represent the speed of light
+wavelength = input("wavelength(if unknown put '?') = ")
+
+if wavelength != "?":
+    wavelength = float(wavelength)
+    frequency = c / wavelength
+    print(f"{frequency=}hz")
+
+else:
+    frequency = float(input("frequency = "))
+    wavelength = c / frequency
+    print(f"{wavelength=}m")
+    
