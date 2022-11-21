025138fe1157b1762f281f47d8b26d2a91256192
TmDobbin <ryandobson@duck.com>
2022-11-21 11:05:18 +0000

    Add diff. code for library or direct execution

--- waveSpeed.py
+++ waveSpeed.py
@@ -1,14 +1,27 @@
-import ecFunctions
 c = 3*10**8 # a constant to represent the speed of light
-wavelength = ecFunctions.inputInt("wavelength(if unknown put '?') = ")
 
-if wavelength != "?":
-    wavelength = float(wavelength)
-    frequency = round((c / wavelength), 3)
-    print(f"{frequency=}hz (3dp)")
+if __name__ == "__main__":
+    import ecFunctions
+    wavelength = input("wavelength(if unknown put '?') = ")
+
+    if wavelength != "?":
+        wavelength = float(wavelength)
+        frequency = round((c / wavelength), 3)
+        print(f"{frequency=}hz (3dp)")
+
+    else:
+        frequency = float(ecFunctions.inputInt("frequency = "))
+        wavelength = round((c / frequency), 3)
+        print(f"{wavelength=}m (3dp)")
 
 else:
-    frequency = float(ecFunctions.inputInt("frequency = "))
-    wavelength = round((c / frequency), 3)
-    print(f"{wavelength=}m (3dp)")
-    
+    def wavespeedCalc(frequency=None, wavelength=None):
+        if wavelength is None:
+            frequency = float(frequency)
+            wavelength = round((c / frequency), 3)
+            return wavelength
+        
+        if wavelength is not None:
+            wavelength = float(wavelength)
+            frequency = round((c / wavelength), 3)
+            return frequency
