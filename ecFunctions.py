5799181fb7ef3611e2e545da43f584113f85a93f
TmDobbin <ryandobson@duck.com>
2022-11-23 10:45:01 +0000

    Delete EquationBuddy 0.0.2.py in favour of equationBuddy.py v0.0.4, define standardForm in ecFunctions

--- ecFunctions.py
+++ ecFunctions.py
@@ -28,3 +28,11 @@ else:
                 valid = False
             
         return userInput
+
+    def standardForm():
+        string = str(answer)
+        round1 = str(round(float(string[:6]), 3))
+        round2 = string[len(string)-3:]
+        return ("E =", round1, "x 10^" + round2)
+		
+		
