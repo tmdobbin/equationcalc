2009dadd440e7f993941282628688f1aa7b33d6a
TmDobbin <ryandobson@duck.com>
2022-11-23 12:09:31 +0000

    Add Standard Form Functions to ecFunctions.py

--- ecFunctions.py
+++ ecFunctions.py
@@ -27,12 +27,26 @@ else:
             except ValueError:
                 valid = False
             
-        return userInput
-
-    def standardForm():
+        return userInput	
+		
+    def standardFormCompToUser(answer):
         string = str(answer)
         round1 = str(round(float(string[:6]), 3))
         round2 = string[len(string)-3:]
-        return ("E =", round1, "x 10^" + round2)
-		
-		
+        return "E =", round1, "x 10^" + round2
+
+    def standardFormUserToInt(sForm):
+        x = sForm.partition(" x 10^")
+        if int(x[2]) < 0:
+            answer = "0."
+            for i in range(abs(int(x[2]))-1):
+                answer += "0"
+            answer += x[0]
+
+        if int(x[2]) > 0:
+            answer = x[0]
+            for i in range(abs(int(x[2]))-1):
+                answer += "0"
+
+        return answer
+
