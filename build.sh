342eeaeb93afb2dd20bf8219c419000e6c24ea44
TmDobbin <ryandobson@duck.com>
2022-12-02 18:03:39 +0000

    Disable ios builds for any platform other than OSX

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
+if [[ $1 == "" ]]; then
+	echo "Please use ./build.sh [OPTION] where OPTION={debug,release}"
+	exit
+fi
 pip3 install kivy
 pip3 install --user --upgrade buildozer
 pip3 install cpython
+cp equationBuddy.py main.py
 buildozer init
-buildozer -v android debug
-buildozer -v ios debug
+echo "Building in $1 mode"
+
+if [[ $1 == "" ]]; then
+	echo ""
+fi
+
+if [[ "OSTYPE" == "darwin"* ]]; then
+	echo "OSTYPE is Darwin. Attempting to build for iOS."
+	buildozer -v ios $1
+else
+	buildozer -v android $1
+fi
 #FOR RELEASE ONLY
 #buildozer -v android release
 #buildozer -v ios release
+
