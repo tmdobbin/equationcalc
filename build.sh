2764f00a389347870ecad4e8f18d3d67b5e0a2f5
TmDobbin <ryandobson@duck.com>
2022-12-02 10:29:15 +0000

    Update build.sh for ios and added (disabled) release build commands

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ pip3 install kivy
 pip3 install --user --upgrade buildozer
 pip3 install cpython
 buildozer init
-buildozer -v android ios debug
-
+buildozer -v android debug
+buildozer -v ios debug
+#FOR RELEASE ONLY
+#buildozer -v android release
+#buildozer -v ios release
