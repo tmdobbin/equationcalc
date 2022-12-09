a1012764342e0640cf79420d89d9f33c6780b37f
TmDobbin <ryandobson@duck.com>
2022-12-09 14:51:13 +0000

    Remove comment from ecFunctions.py

--- ecFunctions.py
+++ ecFunctions.py
@@ -1,5 +1,4 @@
 #A number of functions intended to be called from other scripts.
-# This comment can be deleted at any time, it is a temporary change to test the apk autobuild
 if __name__ == "__main__":
     print("This is a library file")
 
