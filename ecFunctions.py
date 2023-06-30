f72a0d4fb0addc79f2464463d12e7672d70cee8b
jeevezz11 <102586393+jeevezz11@users.noreply.github.com>
2023-06-30 11:35:05 +0100

    Update ecFunctions.py

--- ecFunctions.py
+++ ecFunctions.py
@@ -50,3 +50,41 @@ else:
 
         return answer
 
+    def roundToNsf(value, nsf): 
+
+        integer_part = math.floor(value)
+        if integer_part > 0:
+            integer_part_len = len(str(integer_part))
+            return round(value, nsf-integer_part_len)
+        else:
+            str_value = str(value)
+            #if of the form "8e-05"
+            if '-' in str_value:
+                index = int(str_value[str_value.find('-')+1:]) - 1
+            else:
+                st = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+                index = next((i for i, ch in enumerate(str(value)) if ch in st), None) - 2
+            return round(value, index+nsf)
+
+    def isFloat(num):
+        try:
+            float(num)
+            return True
+        except ValueError:
+            return 
+
+    def getValue():
+    
+        valid = False
+        while valid == False:
+            
+            try:
+                value = float(input())
+                if value > 0:
+                    valid = True
+                else:
+                    print("You must enter a positive number")
+
+            except:
+                print("You must enter a positive number without any characters or symbols")
+        return value
